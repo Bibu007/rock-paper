1574ac7aa5299e5384454581480dbb357e3f4ee7
Bibu007 <bibinbabu218@gmail.com>
2026-04-09 19:10:52 +0530

    Added playRound() function

--- js/script.js
+++ js/script.js
@@ -8,4 +8,46 @@ function getHumanChoice(){
     let str = prompt("Enter your choice");
     let humanChoice = String(str);
     humanChoice = humanChoice.toLowerCase();
+    return humanChoice;
+}
+
+function playRound(){
+    let humanChoice = getHumanChoice();
+    let computerChoice = getComputerChoice();
+    let human = 0;
+    let computer = 0;
+
+    if (humanChoice == "rock" && computerChoice == "paper"){
+        alert("You lose! paper beats rock");
+        computer++;
+    }
+
+    else if (humanChoice == "paper" && computerChoice == "rock"){
+        alert("You win! Paper beats rock");
+        human++;
+    }
+
+    else if (humanChoice == "rock" && computerChoice == "scissors"){
+        alert("You win! Rock beats scissors");
+        human++;
+    }
+
+    else if (humanChoice == "scissors" && computerChoice == "rock"){
+        alert("You lose! Rock beats paper");
+        computer++;
+    }
+
+    else if (humanChoice == "paper" && computerChoice == "scissors"){
+        alert("You lose! Scissors beats paper");
+        computer++;
+    }
+
+    else if (humanChoice == "scissors" && computerChoice == "paper"){
+        alert("You win! Scissors beats rock");
+        human++;
+    }
+
+    else{
+        alert("Try again :(");
+    }
 }
